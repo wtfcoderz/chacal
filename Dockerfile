8b6d76d6a979926e3a88e35eb18d6bee63460d62
WTFKr0 <thomas.kovatchitch@gmail.com>
2017-08-31 14:06:16 +0200

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
+FROM      golang:1.8 as build
+WORKDIR   /go/src
+ENV       CGO_ENABLED=0
+ENV       GO_PATH=/go/src
+RUN       go get github.com/gorilla/mux
+COPY      api /go/src
+RUN       go build -a --installsuffix cgo --ldflags=-s -o chacal
+
 FROM        scratch
-COPY        chacal /
+COPY        --from=build /go/src/chacal /
 ENTRYPOINT  ["/chacal"]
 EXPOSE      80
--- api/chacal.go
+++ api/chacal.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "github.com/gorilla/mux"
-  "net/http"
-  "log"
-  "os"
+	"flag"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
 )
 
 var port string
 
 func init() {
-  flag.StringVar(&port, "port", "80", "give me a port number")
+	flag.StringVar(&port, "port", "80", "give me a port number")
 }
 
 func main() {
-  flag.Parse()
-  r := mux.NewRouter()
-  r.HandleFunc("/health", health)
-  http.Handle("/", r)
-  fmt.Println("Starting up on port " + port)
-  log.Fatal(http.ListenAndServe(":"+port, nil))
+	flag.Parse()
+	r := mux.NewRouter()
+	r.HandleFunc("/health", health)
+	http.Handle("/", r)
+	fmt.Println("Starting up on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func health(w http.ResponseWriter, req *http.Request) {
-  hostname, _ := os.Hostname()
-  fmt.Fprintln(w, "Hostname:", hostname)
+	hostname, _ := os.Hostname()
+	fmt.Fprintln(w, "Hostname:", hostname)
 }
